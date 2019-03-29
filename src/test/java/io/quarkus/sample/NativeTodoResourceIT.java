7e1d3735a8aeb7931a2b96749d2ee4a3691d9f7e
Clement Escoffier <clement.escoffier@gmail.com>
2019-03-29 18:07:48 +0100

    Remove tests :-D

--- src/test/java/io/quarkus/sample/NativeTodoResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.quarkus.sample;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeTodoResourceIT extends TodoResourceTest {
-
-    // Execute the same tests but in native mode.
-}
